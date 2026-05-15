1320ffc0d3532b16e9242522ef0f0e9ad9534eca
Er. Saloni <salonigorasiya.7906@gmail.com>
2026-05-16 02:32:07 +0530

    Fix merge conflict in run.py

--- run.py
+++ run.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-=======
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
->>>>>>> b023fec76e75c42427dcf036c1095c4f5b566ffd
