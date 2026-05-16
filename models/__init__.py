09f2500df72ae58213acbfc6315272930ed31cea
MsGorsiya <gorsiyasaloni@gmail.com>
2026-05-16 12:55:21 +0530

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .student import Students
-from .list_colleges import EngineeringCollege
-from .company import Company
-from .job_post import Job
-from .project import Projects
-from .skills import Skills
-from .certificate import certificate
